449ef159cc7f19fa3ec5366acf91bcba19969564
whmsft <whmsft@outlook.com>
2023-04-18 17:51:04 +0530

    ..

---
+++ wake/www/js/gameManager.js
@@ -0,0 +1,16 @@
+var res = {
+  BG_IMAGE: 'images/BG-HD.png',
+  FLOOR_IMAGE: 'images/Floor-HD.png',
+  ROBIN_IMAGE: 'images/Robin-HD.png',
+};
+
+var kZindexBG = 0;
+var kZindexFloor = 40;
+var kZindexRobin = 100;
+
+var kRobinStateStopped = 0;
+var kRobinStateMoving = 1;
+var kRobinStartSpeedY = 300;
+var kRobinStartX = 240;
+
+var GRAVITY = -620;
