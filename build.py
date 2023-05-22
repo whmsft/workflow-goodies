966c072073a39fa82085e2f2abd58e99033aa963
whmsft <whmsft@outlook.com>
2023-05-22 07:32:19 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,4 +1,6 @@
 import os
+
+print(" -- directory setup --")
 os.system("""
   mkdir --parents android/sdk android/build assets include lib/armeabi-v7a lib/arm64-v8a lib/x86 lib/x86_64 src
 cd android/build
@@ -7,4 +9,6 @@ mkdir --parents lib/armeabi-v7a lib/arm64-v8a lib/x86 lib/x86_64
 mkdir --parents res/drawable-ldpi res/drawable-mdpi res/drawable-hdpi res/drawable-xhdpi
 cd ../..
 """)
+
+print(" -- commandlinetools --")
 os.system('curl --silent "https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip" > clt.zip')
