1209210b2e516c73d983e42712f85a48b79e873d
whmsft <whmsft@outlook.com>
2023-05-22 07:46:08 +0530

    Update build.py

--- build.py
+++ build.py
@@ -13,3 +13,13 @@ cd ../..
 print(" -- commandlinetools --")
 os.system('curl --silent "https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip" > clt.zip')
 os.system("unzip clt.zip -d android/sdk")
+
+print(" -- clt sdkman --")
+os.system("""
+cd android/sdk/cmdline-tools/bin
+./sdkmanager --update --sdk_root=../..
+./sdkmanager --install "build-tools;29.0.3" --sdk_root=../..
+./sdkmanager --install "platform-tools" --sdk_root=../..
+./sdkmanager --install "platforms;android-29" --sdk_root=../..
+cd ../../../..
+""")
