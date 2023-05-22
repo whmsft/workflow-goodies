8df416a0941de368fa5ebd860c4214db122956cd
whmsft <whmsft@outlook.com>
2023-05-22 07:22:11 +0530

    one step

--- build.py
+++ build.py
@@ -1,3 +1,10 @@
 import os
-os.system()
+os.system("""
+  mkdir --parents android/sdk android/build assets include lib/armeabi-v7a lib/arm64-v8a lib/x86 lib/x86_64 src
+cd android/build
+mkdir --parents obj dex res/values src/com/raylib/game assets
+mkdir --parents lib/armeabi-v7a lib/arm64-v8a lib/x86 lib/x86_64
+mkdir --parents res/drawable-ldpi res/drawable-mdpi res/drawable-hdpi res/drawable-xhdpi
+cd ../..
+""")
 os.system('curl --silent "https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip" > clt.zip')
