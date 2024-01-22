9b86fe451f5553603de07f0a6859c6c8a624aff9
whmsft <whmsft@outlook.com>
2024-01-22 17:23:01 +0530

    Another push

--- main.c
+++ main.c
@@ -10,6 +10,6 @@ int main(void) {
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
-    CloseWindow();        // Close window and OpenGL context
+    CloseWindow();
     return 0;
 }
