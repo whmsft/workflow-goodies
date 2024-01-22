f3f2cafff7e85a21630505994b0b847945461f78
whmsft <whmsft@outlook.com>
2024-01-22 17:34:14 +0530

    Another push

--- main.c
+++ main.c
@@ -1,15 +1,30 @@
-#include "./include/raylib.h"
+#include "raylib.h"
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+void UpdateDrawFrame(void);
+
 int main(void) {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-    SetTargetFPS(60);
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-        EndDrawing();
-    }
+    #if defined(PLATFORM_WEB)
+        emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+    #else
+        SetTargetFPS(60);
+        while (!WindowShouldClose()) {
+            UpdateDrawFrame();
+        }
+    #endif
     CloseWindow();
     return 0;
+}
+
+void UpdateDrawFrame(void) {
+    BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+    EndDrawing();
 }
